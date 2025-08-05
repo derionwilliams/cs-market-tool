040729b20a72b2fba9faccdcdd53a41992330bd7
Carlos Figueroa-Diaz <carlos.figueroadiaz@utexas.austin.edu>
2025-08-04 20:58:27 -0500

    added enums

--- formatLink.py
+++ formatLink.py
@@ -1,15 +1,23 @@
+from enum import Enum
 import pandas as pd
 
-def formatLink(type: str, weaponName: str, skinName: str, quality: str,  isSouvenir: bool, isStatTrak: bool):
+class cs2ObjectType(Enum):
+    case = 0
+    gun = 1
+    knife = 2
+    sticker = 3
+
+# Readies links for API retrieval
+def formatLink(type: cs2ObjectType, weaponName: str, skinName: str, quality: str,  isSouvenir: bool, isStatTrak: bool):
     urlBase = "https://steamcommunity.com/market/pricehistory/?appid=730&market_hash_name="
     statTrakText = "StatTrak™"
     knifeText = "★"
 
     # Cases/stickers return a simple link
-    if type == "case" or type == "sticker":
+    if type == cs2ObjectType.case or type == cs2ObjectType.sticker:
         return urlBase + weaponName
     # Weapons get more detailed
-    if type == "knife":
+    if type == cs2ObjectType.knife:
         urlBase += knifeText + " "
     if isSouvenir:
         urlBase += "Souvenir "
@@ -17,12 +25,12 @@ def formatLink(type: str, weaponName: str, skinName: str, quality: str,  isSouve
         urlBase += statTrakText + " "
     return urlBase + weaponName + " | " + skinName + " (" + quality + ")"
 
+# Split weapon + skin name into separate columns
 def seperateNames(rowElement):
     weaponList = ["Glock-18", "P2000", "USP-S", "Dual Berettas", "P250", "Tec-9", "CZ75-Auto", "Five-SeveN", "Desert Eagle", "R8 Revolver",
                  "MAC-10", "MP9", "MP7", "MP5-SD", "UMP-45", "P90", "PP-Bizon", "Nova", "XM1014", "Sawed-Off", "MAG-7", "M249", "Negev", "Galil AR",
                  "FAMAS" , "AK-47", "M4A4", "M4A1-S", "SSG 08", "SG 553", "AUG", "AWP", "G3SG1", "SCAR-20", "Zeus x27"]
     splitText = rowElement.split(" ")
-    print(splitText)
     if splitText[0] in weaponList:
         weaponName = splitText[0]
         skinName = " ".join(splitText[1:])
@@ -32,15 +40,15 @@ def seperateNames(rowElement):
         skinName = " ".join(splitText[2:])
         return (weaponName, skinName)
 
+# Creates a spreadsheet of all weapon skins in cs2
 def fixSpreadSheet():
     sheet = "weaponSkins.csv"
     skinList = pd.read_csv(sheet)
     for index, row in skinList.iterrows():
         separatedNames = seperateNames(skinList.iloc[index, 0])
-        skinList.iloc[index, 0] = separatedNames[0] 
-        skinList.iloc[index, 1] = separatedNames[1]
+        skinList.iloc[index, 0] = separatedNames[0] # type: ignore
+        skinList.iloc[index, 1] = separatedNames[1] # type: ignore
     return skinList.to_csv(sheet, index = False)
-
 
 def writeItemEntry():
     # 1. Access the skin list
@@ -50,6 +58,5 @@ def writeItemEntry():
     # 5. Access the dictonary and contain the data in a spreadsheet for further development
     return
 
-#print(formatLink("knife", "Karambit", "Fade", "Factory New", False, True))
 fixSpreadSheet()
 
