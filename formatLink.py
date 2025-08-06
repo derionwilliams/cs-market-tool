770399e9068e3eb5608a46c418abed1022b7e84a
caf3676 <caf3676@utexas.austin.edu>
2025-08-05 20:50:06 -0500

    MORE CHANGES

--- formatLink.py
+++ formatLink.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import pandas as pd
+import pandas as pd # type: ignore
 
 class cs2ObjectType(Enum):
     case = 0
@@ -50,13 +50,61 @@ def fixSpreadSheet():
         skinList.iloc[index, 1] = separatedNames[1] # type: ignore
     return skinList.to_csv(sheet, index = False)
 
+def collectionIsSouvenir(collection: str):
+    souvenirKeywords = ["Dust 2", "Mirage", "Inferno", "Nuke", "Train", "Cobblestone", "Overpass",
+                        "Cache", "Safehouse", "Lake", "Italy", "Anubis", "Ancient", "Vertigo"]
+    for word in souvenirKeywords:
+        if word in collection:
+            return True
+    return False
+
 def writeItemEntry():
+    class columns(Enum):
+        weaponName = 0
+        skinName = 1
+        rarity = 2
+        collection = 3
+        release = 4
+
+    qualities = ["Battle Scarred", "Field-Tested", "Well-Worn", "Minimal Wear", "Factory New"]
     # 1. Access the skin list
+    sheet = "weaponSkins.csv"
+    skinList = pd.read_csv(sheet)
     # 2. Select a row, grab type, weaponName, and skinName 
+    for index, row in skinList.iterrows():
+        weaponName = str(skinList.iloc[index, columns.weaponName.value])
+        skinName = str(skinList.iloc[index, columns.skinName.value])
+        collection = str(skinList.iloc[index, columns.collection.value])
     # 3. generate a list of formattedLinks by by rarity. Bear in mind you have to distinguish between souvenir and statTrak
-    # 4. Iteratively make an API request for each formattedLink, (non-existent floats are an edge case here)
+        baseLinks, souvenirLinks, statTrakLinks = [], [], []
+        for i in range(len(qualities)):
+            baseLinks.append(formatLink(cs2ObjectType.gun, weaponName, skinName, qualities[i], False, False))
+            if collectionIsSouvenir(collection):
+                souvenirLinks.append(formatLink(cs2ObjectType.gun, weaponName, skinName, qualities[i], True, False))
+                
+                # TESTING
+                # print(baseLinks)
+                # print(souvenirLinks)
+                # return
+            else:
+                statTrakLinks.append(formatLink(cs2ObjectType.gun, weaponName, skinName, qualities[i], False, True))
+        
+        # TESTING
+        # print(baseLinks) 
+        # print(souvenirLinks)
+        # print(statTrakLinks)
+        # return
+
+    # 4. Iteratively make an API request for each formattedLink, a lot of these will not work
+
+    #IDEAS
+    # Suppose we get a set number of faulty requests. When a limit is reached, check the end points of each array to see
+    # where we left off. Get that index/location of the last good request
+    #  and return it. Use as input of function as starting point for link
+    # generation.
+
     # 5. Access the dictonary and contain the data in a spreadsheet for further development
     return
 
-fixSpreadSheet()
-
+#fixSpreadSheet()
+writeItemEntry()
